dfa879730a82b91b34d58400a2267df0b1a3af7a
brenndha-cabral <brenndha.cabral@yahoo.com>
2022-03-28 15:51:46 -0300

    Install dependecies redux

---
+++ src/redux/reducers/user.js
@@ -0,0 +1,20 @@
+// import { LOGIN_INFOS } from '../actions/actions';
+
+// const INITIAL_STATE = {
+//   email: '',
+// };
+
+// const user = (state = INITIAL_STATE, action) => {
+//   switch (action.type) {
+//   case LOGIN_INFOS:
+//     return {
+//       ...state,
+//       ...action.payload,
+//     };
+
+//   default:
+//     return state;
+//   }
+// };
+
+// export default user;
